b82e17b0b617a2806c73c211a7a62b686270d5f3
Samuel.mahembe <samuel.r.mahembe@gmail.com>
2025-07-03 10:03:44 +0200

    implemented upload page

--- frontend/src/app/pages/call-management/upload-call/upload-call.component.ts
+++ frontend/src/app/pages/call-management/upload-call/upload-call.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatChipsModule} from "@angular/material/chips";
+import {TablerIconsModule} from "angular-tabler-icons";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-upload-call',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, TablerIconsModule, MatButtonModule],
   templateUrl: './upload-call.component.html',
   styleUrl: './upload-call.component.scss'
 })
